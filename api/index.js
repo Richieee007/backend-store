bc15432d22f8dfef605c529258dd0dd1935ab79e
Richard Ojok <99288877+Richieee007@users.noreply.github.com>
2024-07-14 23:03:44 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,10 +16,7 @@ app.use(cors({
 }));
 
 //DataBase Connect With MongoDB
-mongoose.connect('mongodb+srv://richieeeojok:j1Md0ChI35o04IME@cluster0.wtdjxfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Ecommerce', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://richieeeojok:j1Md0ChI35o04IME@cluster0.wtdjxfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Ecommerce')
 
 
 
